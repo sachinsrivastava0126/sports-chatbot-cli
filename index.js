510c91bd642d021b45c894cd0c2eac1681278671
Sachin Srivastava <sachinsrivastava0126@gmail.com>
2019-10-22 23:49:12 -0400

    Demo working.

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+'use strict';
+
+const GOOGLE_NEWS_API_BASE_URL = 'https://newsapi.org/v2/';
+const GOOGLE_NEWS_API_KEY = 'e4c8bfe9aacf4ec0982da7220a8ca021';
+
 const
   request = require('request');
+
+var colors = require('colors');
 
 
 
 
 // Get process.stdin as the standard input object.
-var standard_input = process.stdin;
+let standard_input = process.stdin;
 
 // Set input character encoding.
 standard_input.setEncoding('utf-8');
 
 // Prompt user to input data in console.
-console.log("Please input text in command line.");
+console.log("Hey Sports Fan, I'm a dumb bot! Ask me stuff.");
 
 // When user input data and click enter key.
 standard_input.on('data', function (data) {
@@ -23,7 +30,25 @@ standard_input.on('data', function (data) {
         process.exit();
     }else
     {
-        // Print user input in console.
-        console.log('User Input Data : ' + data);
+        
+
+        let team = data.split('the ')[1].split(' ')[0];
+
+        request({
+                 "url": `${GOOGLE_NEWS_API_BASE_URL}top-headlines?apiKey=${GOOGLE_NEWS_API_KEY}&country=us&category=sports&pageSize=1&q=`+team,
+                 "method": "GET"
+             }, (err, res, body) => {
+
+                let messageText = JSON.parse(body).articles[0].content.split(' [')[0].brightWhite.bgBlue;
+                let messageURL = JSON.parse(body).articles[0].url.underline.brightRed;
+
+
+                 console.log("\n\nCHECK IT OUT:".green.bold+"\n"+messageURL+"\n"+messageText+"\n^Read more at link above^\n".zebra+"\n\n");
+
+                 if (err){
+                     console.log("News API Error: ", err);
+                 }
+
+        });
     }
 });
